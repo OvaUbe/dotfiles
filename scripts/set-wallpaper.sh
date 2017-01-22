e3a39f40fbdfdc7d1d1861922b4c5c6f69660447
OvaUbe <trevorismay@gmail.com>
2017-01-22 23:53:01 +0300

    Fix set-wallpaper

--- scripts/set-wallpaper.sh
+++ scripts/set-wallpaper.sh
@@ -6,7 +6,7 @@ set_wallpaper="feh --bg-scale"
 
 contains=`grep "$set_wallpaper" $autorun`
 if [ -z "$contains" ]; then
-    echo "$set_wallpaper $wallpaper" >> $autorun
+    echo "$set_wallpaper $wallpaper &" >> $autorun
 else
-    sed -ie "s/$set_wallpaper .*$/$set_wallpaper ${wallpaper//\//\\/}/" $autorun
+    sed -ie "s/$set_wallpaper.*$/$set_wallpaper ${wallpaper//\//\\/} \&/" $autorun
 fi
