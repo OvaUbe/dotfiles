3bfa31fb965e66b5801d2a1398a47f82fb702fdf
OvaUbe <trevorismay@gmail.com>
2017-01-22 23:50:24 +0300

    Fix autorun

--- scripts/autorun.sh
+++ scripts/autorun.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 feh --bg-scale ~/dotfiles/look/Wallpapers/solid-2A2826-1920x1080.jpg &
-gifbg.sh ~/dotfiles/look/Wallpapers/beesnbombs-necklace.gif &
 telegram &
 deluge &
-cbatticon &
 nm-applet &
 xset b off &
-feh --bg-scale 
