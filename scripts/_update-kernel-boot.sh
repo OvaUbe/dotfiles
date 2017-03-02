fafa7af07d41e259916b3002911bdb2fcae0a068
Vladimir Golubev <vladimir.golubev@iksatprof.ru>
2017-03-02 15:04:07 +0300

    Add update-kernel-boot.sh

---
+++ scripts/_update-kernel-boot.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+esp="/boot"
+image_prefix="vmlinuz-"
+initrd_prefix="initrd.img-"
+boot_options="root=/dev/sda5 resume=/dev/sda2 loglevel=3 rw"
+
+make_entry() {
+    version=${1}
+    title="linux-${version}"
+    file="${esp}/loader/entries/${title}.conf"
+
+    echo "title ${title}" > ${file}
+    echo "linux /${image_prefix}${version}" >> ${file}
+    echo "initrd /${initrd_prefix}${version}" >> ${file}
+    echo "options ${boot_options}" >> ${file}
+}
+
+mkdir -vp ${esp}/loader/entries
+
+for image_path in ${esp}/${image_prefix}*; do
+    image_file=${image_path##*/}
+    version=${image_file#${image_prefix}}
+    initrd_file="${initrd_prefix}${version}"
+    initrd_path="${esp}/${initrd_file}"
+
+    if [ -e ${initrd_path} ]; then
+        echo "Generating for version ${version}"
+    else
+        echo "Skipping version ${version}: initrd not found"
+        continue
+    fi
+
+    make_entry ${version}
+done
+
+bootctl --path=${esp} update
---
+++ scripts/update-kernel-boot.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo _update-kernel-boot.sh
