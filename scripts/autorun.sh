1de38c1a7ccce9b3787a3e602ecd07f45ff93220
ovaube <trevorismay@gmail.com>
2018-03-03 20:09:11 +0300

    New bg.

--- scripts/autorun.sh
+++ scripts/autorun.sh
@@ -2,7 +2,8 @@
 
 create-status-fifo.sh screencapture
 
-feh --bg-scale ~/dotfiles/look/Wallpapers/aoi-bungaku-1.png
+gifbg.sh ~/dotfiles/look/Wallpapers/beesnbombs-flower.gif &
+feh --bg-scale ~/dotfiles/look/Wallpapers/solid-EEEEEE-1920x1080.jpg
 deluge &
 nm-applet &
 xset b off &
