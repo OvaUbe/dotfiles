4ae14ab3256e277b32c25a3cf6b6ffb2b722f282
ovaube <trevorismay@gmail.com>
2018-03-17 23:16:46 +0300

    Get rid of gidbg for now, because it's too heavy on cpu.

--- scripts/autorun.sh
+++ scripts/autorun.sh
@@ -2,7 +2,6 @@
 
 create-status-fifo.sh screencapture
 
-gifbg.sh ~/dotfiles/look/Wallpapers/beesnbombs-flower.gif &
 feh --bg-scale ~/dotfiles/look/Wallpapers/solid-EEEEEE-1920x1080.jpg
 nm-applet &
 xset b off &
