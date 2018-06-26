b4ff4e4326515347bb7522e88e99e1dd551e5e5a
subsinthe <subsinthe@gmail.com>
2018-06-26 21:41:28 +0300

    Get rid of legacy Trolltech.conf.

--- deploy.sh
+++ deploy.sh
@@ -75,8 +75,6 @@ ln    -vfs ${PWD}/look/.gtkrc-2.0           ~/.gtkrc-2.0
 ln    -vfs ${PWD}/look/.Xdefaults           ~/.Xdefaults
 ln    -vfs ${PWD}/look/.xinitrc             ~/.xinitrc
 
-ln    -vfs ${PWD}/look/Trolltech.conf       ~/.config/Trolltech.conf
-
 chmod +x   ${PWD}/scripts/*
 for script in ${PWD}/scripts/*; do
     sudo ln -vfs "$script" /bin/${script##*/}
