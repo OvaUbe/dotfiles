17dc214f07b0e468e85cb6a792efe112ae0fd766
subsinthe <subsinthe@gmail.com>
2018-06-27 22:19:24 +0300

    Set rtags the normal way.

--- starter-kit.sh
+++ starter-kit.sh
@@ -12,12 +12,6 @@ cd ~/
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 cd $PREV
 
-git clone --recursive https://github.com/Andersbakken/rtags.git
-cd rtags
-cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 .
-make
-sudo make install
-
 for package in ${PWD}/deb/*; do
     sudo dpkg -i ${package}
 done
