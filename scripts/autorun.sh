79b944ff8efff6debe9fd681ef18e9bc0dfab32e
subsinthe <subsinthe@gmail.com>
2018-09-05 23:46:29 +0300

    New wallpaper

--- scripts/autorun.sh
+++ scripts/autorun.sh
@@ -3,6 +3,6 @@
 create-status-fifo.sh screencapture
 create-status-fifo.sh mouse '1'
 
-feh --bg-scale ~/dotfiles/look/Wallpapers/particles.jpg
+feh --bg-scale ~/dotfiles/look/Wallpapers/44448476472_ff77fa00fd_o.jpg
 nm-applet &
 xset b off &
