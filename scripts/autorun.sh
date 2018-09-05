2ce8b938c511204d7ceb82ccc11c6f8b1e3a5167
subsinthe <subsinthe@gmail.com>
2018-09-06 00:31:20 +0300

    Change wallpaper

--- scripts/autorun.sh
+++ scripts/autorun.sh
@@ -3,7 +3,7 @@
 create-status-fifo.sh screencapture
 create-status-fifo.sh mouse '1'
 
-feh --bg-scale ~/dotfiles/look/Wallpapers/44448476472_ff77fa00fd_o.jpg
+feh --bg-scale ~/dotfiles/look/Wallpapers/43780441264_4dae593330_o.jpg
 nm-applet &
 xset b off &
 unclutter -idle 2 &
