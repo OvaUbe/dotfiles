21419a6ee785b31c2cce8f57f4fd9dbe7dba5b34
subsinthe <subsinthe@gmail.com>
2018-09-06 15:22:09 +0300

    autorun: Get rid of useless cluster fuse mounts

--- scripts/autorun.sh
+++ scripts/autorun.sh
@@ -6,6 +6,4 @@ create-status-fifo.sh mouse '1'
 feh --bg-scale ~/dotfiles/look/Wallpapers/43780441264_4dae593330_o.jpg
 nm-applet &
 xset b off &
-yt-fuse banach /mnt/banach --enable-logging 2> ~/.yt-fuse/mnt-banach.log &
-yt-fuse freud /mnt/freud --enable-logging 2> ~/.yt-fuse/mnt-freud.log &
 unclutter -idle 2 &
