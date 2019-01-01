9720f8a2c8e981fef5df0d1ab1e5ba6c7ad767e1
subsinthe <subsinthe@gmail.com>
2019-01-02 00:35:01 +0300

    Don't install debs

--- starter-kit.sh
+++ starter-kit.sh
@@ -12,8 +12,4 @@ cd ~/
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 cd $PREV
 
-for package in ${PWD}/debs/*; do
-    sudo dpkg -i ${package}
-done
-
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
