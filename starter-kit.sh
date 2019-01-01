93e2759ed063f629895dc9ce351a5fd886855659
subsinthe <subsinthe@gmail.com>
2019-01-02 00:35:35 +0300

    Drop debs

--- starter-kit.sh
+++ starter-kit.sh
@@ -6,7 +6,3 @@ PREV=$PWD
 cd ~/
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 cd $PREV
-
-for package in ${PWD}/debs/*; do
-    sudo dpkg -i ${package}
-done
